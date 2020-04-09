b584834f4f578e762674894b9d3134c35f6c6ff9
David Evans <evansdav.a@gmail.com>
2020-04-09 11:53:23 -0700

    Completed file 4 with api adjustments

--- challenges/challenge4.js
+++ challenges/challenge4.js
@@ -2,76 +2,83 @@
  *******************************************************************************
  * INSTRUCTIONS:
  * Follow the steps below and answer the discusssion questions that follow.
- * 
+ *
  * 1. Read over the code that follows. `makePromise` is a function that makes
  *    an API call and returns a Promise containing the result. Uncomment code
  *    block #1 and run the code. What happens? (HINT: You may need to run
  *    `npm init` first.)
- * 
- * 
+ *
+ * It printed 'bulbasaur'
+ *
  * 2. Sometimes, when making API calls, we want to make a bunch of calls in
  *    parallel and don't care in what order they resolve. (In other words, they
  *    don't depend on each other.)
- * 
- *    Uncomment code block #2 and run the code. What happens? What advantages 
+ *
+ *    Uncomment code block #2 and run the code. What happens? What advantages
  *    does `Promise.all` give us when dealing with promises?
- * 
- * 
- * 3. Make another variable `planet1Promise` and assign to it the result of
- *    calling `makePromise` with the URL `https://swapi.co/api/planets/1`.
+ *
+ * All three api calls are made asynchronously, and the three results are
+ * printed at about the same time. Promise.all lets us resolve several
+ * promises at once, without waiting for one to complete for the next to run
+ *
+ * 3. Make another variable `location1Promise` and assign to it the result of
+ *    calling `makePromise` with the URL `https://pokeapi.co/api/v2/location/1.
  *    Add it to the array passed to `Promise.all`, then print the name of the
- *    returned planet inside the `.then()` callback.
- * 
- * 
+ *    returned location inside the `.then()` callback.
+ *
+ *
  *******************************************************************************
  */
 
-const request = require('request');
-
+const request = require('request')
 
 /**
  * Makes an API request and returns a Promise containing the result.
  * @param url The url to make an API request to.
  */
-function makePromise(url) {
-    return new Promise(function(resolve, reject){
-        request(url, function (err, response, body) {
-            if (err) {
-                // Deal with any errors from the API call
-                return reject(err);
-            } else {
-                try {
-                    // If this works, then - success!! Return the parsed body.
-                    resolve(JSON.parse(body))
-                } catch (err) {
-                    // Deal with any errors from parsing the response
-                    return reject(err);
-                }
-            }
-        });
-    });
+function makePromise (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, response, body) {
+      if (err) {
+      // Deal with any errors from the API call
+        return reject(err)
+      } else {
+        try {
+          // If this works, then - success!! Return the parsed body.
+          resolve(JSON.parse(body))
+        } catch (err) {
+          // Deal with any errors from parsing the response
+          return reject(err)
+        }
+      }
+    })
+  })
 }
 
-const person1Promise = makePromise('https://swapi.co/api/people/1')
-const person2Promise = makePromise('https://swapi.co/api/people/2')
-const person3Promise = makePromise('https://swapi.co/api/people/3')
+// SWAPI no longer maintained, Pokeapi recommended instead
+const pokemon1Promise = makePromise('https://pokeapi.co/api/v2/pokemon/1')
+const pokemon2Promise = makePromise('https://pokeapi.co/api/v2/pokemon/2')
+const pokemon3Promise = makePromise('https://pokeapi.co/api/v2/pokemon/3')
+const location1Promise = makePromise('https://pokeapi.co/api/v2/location/1')
 
 /* Uncomment me! #1 */
-// person1Promise.then(function(personResult) {
-//     console.log(`Resulting person's name: ${personResult.name}`);
-// }).catch(function(err) {
-//     console.log("Got an error!")
-//     console.log(err);
-// });
+pokemon1Promise.then(function (pokemonResult) {
+  console.log(`Resulting pokemon's name: ${pokemonResult.name}`)
+}).catch(function (err) {
+  console.log('Got an error!')
+  console.log(err)
+})
 
 /* Uncomment me! #2 */
-// Promise.all([person1Promise, person2Promise, person3Promise])
-//     .then(function(results) {
-//         for (let i = 0; i < 3; i++) {
-//             console.log(`Person ${i+1}'s name: ${results[i].name}`)
-//         }
-//     })
-//     .catch(function(err) {
-//         console.log('Got an error!')
-//         console.log(err)
-//     })
+Promise.all([pokemon1Promise, pokemon2Promise,
+  pokemon3Promise, location1Promise])
+  .then(function (results) {
+    for (let i = 0; i < 3; i++) {
+      console.log(`Pokemon ${i + 1}'s name: ${results[i].name}`)
+    }
+    console.log(`Location 1's name: ${results[3].name}`)
+  })
+  .catch(function (err) {
+    console.log('Got an error!')
+    console.log(err)
+  })
